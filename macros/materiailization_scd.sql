cfff47034f48d2ca3ad4f9cfeee5d9f6b01113c5
Mallegrissimo <martin.job8@gmail.com>
2023-05-29 21:17:08 +1200

    simplified param with scd_settings

--- macros/materiailization_scd.sql
+++ macros/materiailization_scd.sql
@@ -1,7 +1,9 @@
 {%- materialization scd, default -%}
   {%- set target_relation = this %}
-  {%- set unique_key = config.get('unique_key', 'unique_key') -%}
-  {%- set updated = config.get('updated', 'updated') -%}
+  {%- set unique_key = config.get('unique_key', []) -%}
+  {%- set unique_keys = [unique_key] if unique_key is string else unique_key %}
+  {%- set unique_key_alias = config.get('unique_key_alias', 'unique_key') -%}
+  {%- set updated_at = config.get('updated_at', 'updated_at') -%}
   {%- set scd1_columns = config.get('scd1_columns', []) -%}
   {%- set scd2_columns = config.get('scd2_columns', []) -%}
   {%- set etl_is_current = config.get('etl_is_current', 'etl_is_current') -%}
@@ -14,8 +16,9 @@
   {%- set hash_columns = [hash1, hash2] if scd1_columns|length > 0 else [hash2] -%}
   {%- set hash_columns_with_etl_columns = hash_columns + etl_columns -%}
 
-  {%- set scd_settings = {'unique_key': unique_key
-                          , 'updated': updated
+  {%- set scd_settings = {'unique_keys': unique_keys
+                          , 'unique_key_alias': unique_key_alias
+                          , 'updated_at': updated_at
                           , 'scd1_columns': scd1_columns
                           , 'scd2_columns': scd2_columns
                           , 'etl_is_current': etl_is_current
@@ -105,122 +108,104 @@
 
 {%- macro _build_full_load(tmp_relation, temp_relation_columns, sql, scd_settings) -%}
   {%- set temp_relation_cols_csv = temp_relation_columns | map(attribute='quoted') | join(', ') -%}
-  {%- set updated = scd_settings.get('updated') -%}
-  {%- set unique_key = scd_settings.get('unique_key') -%}
-  {%- set etl_is_current = scd_settings.get('etl_is_current') -%}
-  {%- set etl_valid_from = scd_settings.get('etl_valid_from') -%}
-  {%- set etl_valid_to = scd_settings.get('etl_valid_to') -%}
-  {%- set hash1 = scd_settings.get('hash1') -%}
-  {%- set hash2 = scd_settings.get('hash2') -%}
-  {%- set record_default_end_date = scd_settings.get('record_default_end_date') %}
-  {%- set scd1_columns = scd_settings.get('scd1_columns') -%}
-  {%- set scd2_columns = scd_settings.get('scd2_columns') -%}
-  {%- set scd1_columns_csv = quoted(scd1_columns) -%}
-  {%- set scd2_columns_csv = quoted(scd2_columns) -%}
-  {%- set is_scd1_enabled = scd1_columns|length > 0  -%}
+  {%- set unique_keys_csv = scd_settings.unique_keys |join(', ') -%}
+  {%- set scd1_columns_csv = quoted(scd_settings.scd1_columns) -%}
+  {%- set scd2_columns_csv = quoted(scd_settings.scd2_columns) -%}
+  {%- set is_scd1_enabled = scd_settings.scd1_columns|length > 0  -%}
 
 WITH src as (
 SELECT *
-  , ROW_NUMBER() OVER (PARTITION BY {{ quoted(unique_key) }} order by {{ quoted(updated) }} DESC) AS rnk
-  {{- ', ' ~hash(scd1_columns, hash1) if is_scd1_enabled }}
-  , {{ hash(scd2_columns, hash2) }}
-  FROM ({{ sql }})
+  , ROW_NUMBER() OVER (PARTITION BY {{ unique_keys_csv }} order by {{ quoted(scd_settings.updated_at) }} DESC) AS rnk
+  {{- ', ' ~ hash(scd1_columns, hash1) if is_scd1_enabled }}
+  , {{ hash(scd_settings.scd2_columns, scd_settings.hash2) }}
+  FROM ({{ sql }}
+  )
 )
 SELECT  {{ temp_relation_cols_csv }}
-  {{- ', ' ~  quoted(hash1) if is_scd1_enabled -}}
-  , {{ quoted(hash2) }}
-  , CASE rnk WHEN 1 THEN 1 ELSE 0 END AS {{ quoted(etl_is_current) }}
-  , {{ quoted(updated) }}             AS {{ quoted(etl_valid_from) }}
-  , LEAD({{ quoted(updated) }}, 1, '{{ record_default_end_date }}') OVER (PARTITION BY {{ quoted(unique_key) }} ORDER BY {{ quoted(updated) }})  
-                                      AS {{ quoted(etl_valid_to) }}
+  {{- ', ' ~  quoted(scd_settings.hash1) if is_scd1_enabled -}}
+  , {{ quoted(scd_settings.hash2) }}
+  , CASE rnk WHEN 1 THEN 1 ELSE 0 END AS {{ quoted(scd_settings.etl_is_current) }}
+  , {{ quoted(scd_settings.updated_at) }}             AS {{ quoted(scd_settings.etl_valid_from) }}
+  , LEAD({{ quoted(scd_settings.updated_at) }}, 1, '{{ scd_settings.record_default_end_date }}') OVER (PARTITION BY {{ unique_keys_csv }} ORDER BY {{ quoted(scd_settings.updated_at) }})  
+                                      AS {{ quoted(scd_settings.etl_valid_to) }}
 FROM src
 {%- endmacro -%}
 
 {%- macro _build_merge_sql(target_relation, target_relation_columns, sql, scd_settings) -%}
-  {%- set updated = scd_settings.get('updated') -%}
-  {%- set unique_key = scd_settings.get('unique_key') -%}
-  {%- set etl_is_current = scd_settings.get('etl_is_current') -%}
-  {%- set etl_valid_from = scd_settings.get('etl_valid_from') -%}
-  {%- set etl_valid_to = scd_settings.get('etl_valid_to') -%}
-  {%- set hash1 = scd_settings.get('hash1') -%}
-  {%- set hash2 = scd_settings.get('hash2') -%}
-  {%- set record_default_end_date = scd_settings.get('record_default_end_date') -%}
-  {%- set scd1_columns = scd_settings.get('scd1_columns') -%}
-  {%- set scd2_columns = scd_settings.get('scd2_columns') -%}
-  {%- set scd1_columns_csv = quoted(scd1_columns) -%}
-  {%- set scd2_columns_csv = quoted(scd2_columns) -%}
-  {%- set is_scd1_enabled = scd1_columns|length > 0  -%}
-  {%- set etl_columns = scd_settings.get('etl_columns') -%}
-  {%- set etl_columns = scd_settings.get('etl_columns') -%}
-  {%- set hash_columns_with_etl_columns = scd_settings.get('hash_columns_with_etl_columns') -%}
+  {%- set unique_keys_csv = scd_settings.unique_keys|join(', ') -%}
+  {%- set scd1_columns_csv = quoted(scd_settings.scd1_columns) -%}
+  {%- set scd2_columns_csv = quoted(scd_settings.scd2_columns) -%}
+  {%- set is_scd1_enabled = scd_settings.scd1_columns|length > 0  -%}
   {%- set relation_cols_csv = quoted(target_relation_columns | map(attribute='column') | list)-%}
-  {%- set target_relation_columns_subset = target_relation_columns| map(attribute='column') | list | reject('in', hash_columns_with_etl_columns|map('upper')|list) | list -%}
+  {%- set target_relation_columns_subset = target_relation_columns| map(attribute='column') | list | reject('in', scd_settings.hash_columns_with_etl_columns|map('upper')|list) | list -%}
   {%- set target_relation_columns_subset_csv = quoted(target_relation_columns_subset, 'src') -%}
 
 MERGE INTO {{ target_relation }}  tgt
 USING (
 SELECT *
-  , ROW_NUMBER() OVER (PARTITION BY {{ quoted(unique_key) }} ORDER BY {{ quoted(updated) }} DESC) AS rnk
-  {{- ', ' ~hash(scd1_columns, hash1) if is_scd1_enabled }}
-  , {{ hash(scd2_columns, hash2) }}
-  FROM ({{ sql }})
-) AS src ON src.{{ quoted(unique_key)}} = tgt.{{ quoted(unique_key)}} AND tgt.{{ quoted(etl_is_current) }} = 1 
-WHEN MATCHED AND src.{{ quoted(hash2) }} <> tgt.{{ quoted(hash2) }} THEN
-    UPDATE SET tgt.{{ quoted(etl_is_current) }} = 0
-      , tgt.{{ quoted(etl_valid_to) }} = src.{{ quoted(updated) }}
+  , ROW_NUMBER() OVER (PARTITION BY {{ unique_keys_csv }} ORDER BY {{ quoted(scd_settings.updated_at) }} DESC) AS rnk
+  {{- ', ' ~ hash(scd_settings.scd1_columns, scd_settings.hash1) if is_scd1_enabled }}
+  , {{ hash(scd_settings.scd2_columns, scd_settings.hash2) }}
+  FROM ({{ sql }}
+  )
+) AS src ON
+  {% for unique_key in scd_settings.unique_keys %}
+  {{ 'AND ' if not loop.first -}} 
+  src.{{ quoted(unique_key)}} = tgt.{{ quoted(unique_key)}}
+  {%- endfor %}
+  AND tgt.{{ quoted(scd_settings.etl_is_current) }} = 1 
+WHEN MATCHED AND src.{{ quoted(scd_settings.hash2) }} <> tgt.{{ quoted(scd_settings.hash2) }} THEN
+    UPDATE SET tgt.{{ quoted(scd_settings.etl_is_current) }} = 0
+      , tgt.{{ quoted(scd_settings.etl_valid_to) }} = src.{{ quoted(scd_settings.updated_at) }}
   {%- if is_scd1_enabled -%}
-WHEN MATCHED AND src.{{ quoted(hash1) }} <> tgt.{{ quoted(hash1) }}   THEN
-    UPDATE SET tgt.{{ quoted(updated) }} = src.{{ quoted(updated) }}
-      , tgt.{{ quoted(hash1) }} = src.{{ quoted(hash1) }}
-      {% for item in scd1_columns -%}
+WHEN MATCHED AND src.{{ quoted(scd_settings.hash1) }} <> tgt.{{ quoted(scd_settings.hash1) }}   THEN
+    UPDATE SET tgt.{{ quoted(scd_settings.updated_at) }} = src.{{ quoted(scd_settings.updated_at) }}
+      , tgt.{{ quoted(scd_settings.hash1) }} = src.{{ quoted(scd_settings.hash1) }}
+      {% for item in scd_settings.scd1_columns -%}
       , tgt.{{quoted(item)}} = src.{{quoted(item)}} 
       {{- ', ' if not loop.last }}
       {%- endfor %}
   {%- endif %}
 WHEN NOT MATCHED THEN
-    INSERT ({{ relation_cols_csv }}) VALUES({{ target_relation_columns_subset_csv }}, {{ quoted(hash2, 'src') }},1, {{ quoted(updated, 'src') }}, '{{ record_default_end_date }}')
+    INSERT ({{ relation_cols_csv }}) 
+    VALUES({{ target_relation_columns_subset_csv }}, {{ quoted(scd_settings.hash2, 'src') }},1, {{ quoted(scd_settings.updated_at, 'src') }}, '{{ scd_settings.record_default_end_date }}')
 
 {%- endmacro -%}
 
 {%- macro _build_insert_sql(target_relation, target_relation_columns, sql, scd_settings) -%}
   {%- set relation_cols_csv = target_relation_columns | map(attribute='quoted') | join(', ') -%}
-  {%- set updated = scd_settings.get('updated') -%}
-  {%- set unique_key = scd_settings.get('unique_key') -%}
-  {%- set etl_is_current = scd_settings.get('etl_is_current') -%}
-  {%- set etl_valid_from = scd_settings.get('etl_valid_from') -%}
-  {%- set etl_valid_to = scd_settings.get('etl_valid_to') -%}
-  {%- set hash1 = scd_settings.get('hash1') -%}
-  {%- set hash2 = scd_settings.get('hash2') -%}
-  {%- set record_default_end_date = scd_settings.get('record_default_end_date') %}
-  {%- set scd1_columns = scd_settings.get('scd1_columns') -%}
-  {%- set scd2_columns = scd_settings.get('scd2_columns') -%}
-  {%- set etl_columns = scd_settings.get('etl_columns') -%}
-  {%- set hash_columns_with_etl_columns = scd_settings.get('hash_columns_with_etl_columns') -%}
-  {%- set scd1_columns_csv = quoted(scd1_columns) -%}
-  {%- set scd2_columns_csv = quoted(scd2_columns) -%}
-  {%- set is_scd1_enabled = scd1_columns|length > 0  -%}
+  {%- set unique_keys_csv = scd_settings.unique_keys|join(', ') -%}
+ 
+  {%- set scd1_columns_csv = quoted(scd_settings.scd1_columns) -%}
+  {%- set scd2_columns_csv = quoted(scd_settings.scd2_columns) -%}
+  {%- set is_scd1_enabled = scd_settings.scd1_columns|length > 0  -%}
   {%- set target_relation_columns_subset = target_relation_columns| map(attribute='column') | list | reject('in', hash_columns_with_etl_columns|map('upper')|list) | list -%}
   {#%- set target_relation_columns_subset = target_relation_columns_subset | reject('in', [hash1]|map('upper')|list) | list -%#}
   {%- set target_relation_columns_subset_csv_w_prefix = quoted(target_relation_columns_subset, 'src') -%}
 
-INSERT INTO {{ target_relation }} ({{ quoted(target_relation_columns_subset) }}, {{  quoted(hash2) }}, {{  quoted(etl_is_current) }}, {{  quoted(etl_valid_from) }}, {{  quoted(etl_valid_to) }})
+INSERT INTO {{ target_relation }} ({{ quoted(target_relation_columns_subset) }}, {{  quoted(scd_settings.hash2) }}, {{  quoted(scd_settings.etl_is_current) }}, {{  quoted(scd_settings.etl_valid_from) }}, {{  quoted(scd_settings.etl_valid_to) }})
 WITH src as ( 
 SELECT *
-  , ROW_NUMBER() OVER (PARTITION BY {{ quoted(unique_key) }} order by {{ quoted(updated) }} DESC) AS rnk
+  , ROW_NUMBER() OVER (PARTITION BY {{ unique_keys_csv }} ORDER BY {{ quoted(scd_settings.updated_at) }} DESC) AS rnk
   {{- ', ' ~hash(scd1_columns, hash1) if is_scd1_enabled }}
-  , {{ hash(scd2_columns, hash2) }}
-  FROM ({{ sql }})
+  , {{ hash(scd_settings.scd2_columns, scd_settings.hash2) }}
+  FROM ({{ sql }}
+  )
 )  
 , tgt as (
   SELECT *
-    ,ROW_NUMBER()  OVER(PARTITION BY s.{{ quoted(unique_key) }} ORDER BY s.{{ quoted(etl_valid_from) }} DESC) rnk 
+    ,ROW_NUMBER()  OVER(PARTITION BY {{ unique_keys_csv }} ORDER BY s.{{ quoted(scd_settings.etl_valid_from) }} DESC) rnk 
   FROM {{ target_relation }} s
   QUALIFY rnk = 1
 )
-SELECT {{ target_relation_columns_subset_csv_w_prefix }}, {{ quoted(hash2, 'src') }},1, {{ quoted(updated, 'src') }}, '{{ record_default_end_date }}'
+SELECT {{ target_relation_columns_subset_csv_w_prefix }}, {{ quoted(scd_settings.hash2, 'src') }},1, {{ quoted(scd_settings.updated_at, 'src') }}, '{{ scd_settings.record_default_end_date }}'
 FROM src
   JOIN tgt 
-    ON src.{{ quoted(unique_key) }} = tgt.{{ quoted(unique_key) }}
-        AND src.{{ quoted(updated) }} = tgt.{{ quoted(etl_valid_to) }}
+    ON 
+    {%- for unique_key in scd_settings.unique_keys %}
+    {{ 'AND ' if not loop.first -}} 
+    src.{{ quoted(unique_key)}} = tgt.{{ quoted(unique_key)}} 
+    {%- endfor %}
+    AND src.{{ quoted(scd_settings.updated_at) }} = tgt.{{ quoted(scd_settings.etl_valid_to) }}
 
 {%- endmacro -%}
